09e22a887ba8cf58309266cc353dd38cd80feb5d
amyleed2 <amy.lee.d2@gmail.com>
2025-11-19 14:08:29 +0900

    first commit

---
+++ DailyRoutine/DailyRoutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyRoutineApp.swift
+//  DailyRoutine
+//
+//  Created by ezyeun on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyRoutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyRoutineTests/DailyRoutineTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyRoutineTests.swift
+//  DailyRoutineTests
+//
+//  Created by ezyeun on 11/19/25.
+//
+
+import Testing
+@testable import DailyRoutine
+
+struct DailyRoutineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
